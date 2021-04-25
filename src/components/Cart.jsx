ca6da82f8302fbdb033b5cde751dd2f01ba3b6ba
Fernando Santacruz <santacruzfernandog@gmail.com>
2021-04-25 13:59:51 -0300

    Agregado el formulario y show order id en Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -8,18 +8,28 @@ import {getFirestore} from '../firebase'
 
 const Cart = () => {
 
+    const [name, setName] = useState('')
+    const [phone, setPhone] = useState('')
+    const [email, setEmail] = useState('')
+    const [idOrder, setIdOrder] = useState(null)
+    const [showForm, setShowForm] = useState(false)
+
+
     const { cart, removeItem, clear } = useContext(CartContext)
     const [totalItems, setTotalItems] = useState(0)
     const [totalPrecio, setTotalPrecio] = useState(0)
 
-    const generarOrden = () => {
+    const generarOrden = (e) => {
+        e.preventDefault()
+        const comprador = { name, phone, email }
+
         const db = getFirestore()
         const ordersCollection = db.collection('orders')
         
         let orden = {}
 
         orden.date = firebase.firestore.Timestamp.fromDate(new Date())
-        orden.buyer = {name: 'Fernando', phone:1522336699, email:'fernando@prueba.com'}
+        orden.buyer = comprador
         orden.total = totalPrecio
         orden.items = cart.map(cartItem => {
             const id = cartItem.item.id
@@ -30,9 +40,9 @@ const Cart = () => {
         })
 
         ordersCollection.add(orden)
-        .then((idDocument)=>{console.log(idDocument.id)})
+        .then((doc)=>{setIdOrder(doc.id)})
         .catch(error => {console.log(error)})
-        .finally(() => {console.log('Promesa concluida')})
+        .finally(() => {console.log('Orden generada')})
 
 
         //Actualizar stock en firebase
@@ -70,8 +80,7 @@ const Cart = () => {
 
 
     return (
-        <div>
-
+        <div className="container-fluid">
             {
                 !cart.length ?
                 <h2>No hay items en el carrito.
@@ -87,7 +96,7 @@ const Cart = () => {
                                 <div>
                                     Cantidad: {elem.quantity}
                                 </div>
-                                <button onClick={() => removeItem(elem.item.id) }>
+                                <button className="btn btn-primary btn-sm" onClick={() => removeItem(elem.item.id) }>
                                     Borrar
                                 </button>
                             </div>
@@ -97,12 +106,50 @@ const Cart = () => {
                         <div>
                             Total: {totalItems} y {totalPrecio}
                         </div>
-                        <button onClick={clear}>
+                        <button className="btn btn-danger btn-sm" onClick={clear}>
                             Borrar todo
                         </button>
-                        <button onClick={generarOrden}>Finalizar Compra</button>
+                        <button className="btn btn-warning btn-sm" onClick={()=> {setShowForm(!showForm)}}>Finalizar Compra</button>
+
+                        {
+                            showForm ? (
+                                <form onSubmit={generarOrden} className="mt-5">
+                                    <div className="form-row">
+                                        <div className="col-md-3">
+                                            <label htmlFor="userName">Nombre</label>
+                                            <input className="form-control" type="text" id="userName" value={name} onChange={ (e)=> setName(e.target.value) }/>
+                                        </div>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="col-md-3 mt-2">
+                                            <label htmlFor="userPhone">Telefono</label>
+                                            <input className="form-control" type="text" id="userPhone" value={phone} onChange={ (e)=> setPhone(e.target.value) }/>
+                                        </div>
+                                    </div>
+                                    <div className="form-row">
+                                        <div className="col-md-3 mt-2">
+                                            <label htmlFor="userEmail">Email</label>
+                                            <input className="form-control" type="text" id="userEmail" value={email} onChange={ (e)=> setEmail(e.target.value) }/>
+                                        </div>
+                                    </div>
+                                    <button className="btn btn-primary mt-3" type="submit">Generar Orden</button>
+                                </form>
+                            ) : null
+                        }
                     </>
                 )
+            }
+
+            {
+                idOrder ? (
+                    <div className="alert alert-success alert-dismissible fade show mt-5 h5" role="alert">
+                        Orden de compra generada correctamente!<br/>
+                        <strong>ID de orden: {idOrder}</strong>
+                        <button type="button" className="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                ) : null
             }
         </div>
     )
